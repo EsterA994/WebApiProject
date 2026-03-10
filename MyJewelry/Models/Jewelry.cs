b277b74b662a7f4370160edafb97fc98ca00ffab
EsterA994 <esty994994@gmail.com>
2026-03-10 15:24:54 +0200

    add jewelaryRepository

--- MyJewelry/Models/Jewelry.cs
+++ MyJewelry/Models/Jewelry.cs
@@ -3,6 +3,7 @@ namespace MyJewelry.Models;
 public class Jewelry
 {
     public int Id { get; set; }
+    public int UserId { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
     public string Category { get; set; }
